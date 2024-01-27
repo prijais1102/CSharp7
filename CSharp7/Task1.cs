9d5d333ff9eaf165fed3f71957c4b0cb8c7013dd
prijais1102 <priyajaiswal.mzp1@gmail.com>
2024-01-28 05:21:17 +0530

    Add project files.

---
+++ CSharp7/Task1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+class Task1
+{
+    static void Main()
+    {
+       
+        Directory.CreateDirectory("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo");
+
+       
+        Directory.CreateDirectory("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo1");
+        Directory.CreateDirectory("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo2");
+
+  
+        File.Create("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo\\file1.txt").Close();
+        new FileInfo("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo\\file2.txt").Create().Close();
+
+        
+        File.WriteAllText("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo\\file1.txt", "hello priya");
+        File.WriteAllText("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo\\file2.txt", "hello priyaaa");
+
+        
+        File.Copy("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo\\file1.txt", "C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo2\\file1.txt");
+
+       
+        var directories = Directory.GetDirectories("C:\\Users\\PriyaJaiswal\\Desktop\\Demo");
+        var files = Directory.GetFiles("C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo");
+
+        Console.WriteLine("Directories:");
+        foreach (var dir in directories)
+        {
+            Console.WriteLine(dir);
+        }
+
+        Console.WriteLine("Files:");
+        foreach (var file in files)
+        {
+           Console.WriteLine(file);
+        }
+
+         
+        foreach (var dir in directories)
+        {
+            Console.WriteLine($"{dir} was created on {Directory.GetCreationTime(dir)}");
+        }
+
+        foreach (var file in files)
+        {
+           Console.WriteLine($"{file} was created on {File.GetCreationTime(file)}");
+        }
+
+
+        foreach (var file in files)
+        {
+            File.Delete(file);
+        }
+
+
+        foreach (var dir in directories)
+        {
+            Directory.Delete(dir, true);
+        }
+    }
+}
+
---
+++ CSharp7/Task2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+class Task2
+{
+    static void Main()
+    {
+        bool condition = true;
+        string filePath = "C:\\Users\\PriyaJaiswal\\Desktop\\Demo\\demo1.txt.txt";
+        while (condition)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Create a new file");
+            Console.WriteLine("2. Add contents to the file");
+            Console.WriteLine("3. Append contents to the file");
+            Console.WriteLine("4. Display contents one by one");
+            Console.WriteLine("5. Display all contents together");
+            Console.WriteLine("6. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FileInfo fileinfo = new FileInfo(filePath);
+                    if (!fileinfo.Exists)
+                    {
+                        using (FileStream fs = File.Create(filePath)) { }
+                        Console.WriteLine("File created.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("File already exists");
+                    }
+                    break;
+                case 2:
+                    Console.Write("Enter content to add: ");
+                    string contentToAdd = Console.ReadLine();
+                    using (StreamWriter sw = new StreamWriter(filePath))
+                    {
+                        sw.WriteLine(contentToAdd);
+                    }
+                    Console.WriteLine("Content added.");
+                    break;
+                case 3:
+                    Console.Write("Enter content to append: ");
+                    string contentToAppend = Console.ReadLine();
+                    using (StreamWriter sw = File.AppendText(filePath))
+                    {
+                        sw.WriteLine(contentToAppend);
+                    }
+                    Console.WriteLine("Content appended.");
+                    break;
+                case 4:
+                    using (StreamReader sr = File.OpenText(filePath))
+                    {
+                        string s;
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                    break;
+                case 5:
+                    string content = File.ReadAllText(filePath);
+                    Console.WriteLine(content);
+                    break;
+                case 6:
+                    condition = false;
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 6.");
+                    break;
+            }
+        }
+    }
+}
